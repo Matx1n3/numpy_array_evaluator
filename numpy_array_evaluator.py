2d48c2df7d834d7a8830c851d4023e01666dc699
Matxin Jimenez <53516864+Matx1n3@users.noreply.github.com>
2023-12-12 22:18:00 +0100

    Add files via upload

---
+++ numpy_array_evaluator.py
@@ -0,0 +1,41 @@
+import concurrent
+import os
+import numpy as np
+import concurrent.futures
+
+
+def evaluate_all_elements(func, arr, args):
+    """
+    Evaluates all elements of a NumPy array using a specified function in parallel processes.
+
+    Parameters:
+    func (function): The function to apply to each element.
+    arr (numpy.ndarray): The input NumPy array.
+    args (tuple): Additional arguments to pass to the function.
+
+    Returns:
+    numpy.ndarray: An array containing the results of applying the function to all elements.
+    """
+    num_cores = os.cpu_count()
+    split_arr = np.array_split(arr, num_cores)
+    args_list = [(func, batch,) + args for batch in split_arr]
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=num_cores) as executor:
+        results = list(executor.map(evaluate_batch, args_list))
+
+    return np.concatenate(results)
+
+
+def evaluate_batch(args):
+    """
+    Evaluates a batch of elements using a specified function.
+
+    Parameters:
+    args (tuple): A tuple containing the function, batch array, and additional function arguments.
+
+    Returns:
+    numpy.ndarray: An array containing the results of applying the function to the batch.
+    """
+    func, batch, *func_args = args
+    return np.apply_along_axis(func1d=func, axis=1, arr=batch, args=func_args)
+
